a75bb3a15db1eee098b21ff7b19064ad15bee6c6
MainMan <your_email@whatever.com>
2017-08-26 20:02:34 +0700

    Add DevTools

--- src/app.js
+++ src/app.js
@@ -1,11 +1,15 @@
 import React, { Component } from 'react';
+import { DevTools } from './utils/index';
 
 export default class App extends Component {
     static path = '/';
 
     render() {
         return (
-            <h1>Hello world!</h1>
+            <div>
+                <h1>Hello world!</h1>
+                { process.env.NODE_ENV !== 'production' ? <DevTools /> : null }
+            </div>
         );
     }
 }
