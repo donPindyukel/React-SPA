f7d92fe6344308738ebd4ed1ec2929d7032c9954
MainMan <your_email@whatever.com>
2017-08-27 18:30:24 +0700

    Add 404 page

--- src/routes.js
+++ src/routes.js
@@ -4,6 +4,7 @@ import App from './app';
 
 import { HomePage, HomeRoutes } from './pages/home';
 import { ContactRoutes } from './pages/contact';
+import ErrorPage from './pages/error/index';
 
 
 export default (
@@ -12,5 +13,7 @@ export default (
         
         { HomeRoutes }
         { ContactRoutes }
+
+        <Route path='*' component={ ErrorPage } />
     </Route>
 );
